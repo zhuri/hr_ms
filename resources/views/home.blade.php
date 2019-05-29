272f799182a0a824c1cb205f100914f62b9b9a0c
zhuri <zhuriendrit@gmail.com>
2019-05-29 16:08:43 +0200

    added chat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <div class="row">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
@@ -26,6 +26,11 @@
                     <br>
                     <div><a href="{{ url('/departments') }}" class="btn btn-default btn-block">Departments</a></div>
                 </div>            
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card card-user">
+                @include('partials.chat')
             </div>
         </div>
     </div>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -41,27 +41,7 @@
                     </div>
                     <div class="col-md-4">
                         <div class="card card-user">
-                            <div class="image">
-                                <img src="https://ununsplash.imgix.net/photo-1431578500526-4d9613015464?fit=crop&fm=jpg&h=300&q=75&w=400" alt="..."/>
-                            </div>
-                            <div class="content">
-                                <div class="author">
-                                     <a href="#">
-                                    <img class="avatar border-gray" src="assets/img/faces/face-3.jpg" alt="..."/>
-    
-                                      <h4 class="title">Mike Andrew<br />
-                                         <small>michael24</small>
-                                      </h4>
-                                    </a>
-                                </div>                            
-                            </div>
-                            <hr>
-                            <div class="text-center">
-                                <button href="#" class="btn btn-simple"><i class="fa fa-facebook-square"></i></button>
-                                <button href="#" class="btn btn-simple"><i class="fa fa-twitter"></i></button>
-                                <button href="#" class="btn btn-simple"><i class="fa fa-google-plus-square"></i></button>
-    
-                            </div>
+                            @include('partials.chat')
                         </div>
                     </div>
                 </div>
