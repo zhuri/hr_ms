7e2fb4382abeb8d5244a71b72ee9da40eae7a9d6
zhuri <zhuriendrit@gmail.com>
2019-06-03 14:12:13 +0200

    emp db

--- emp_ms.sql
+++ emp_ms.sql
@@ -332,7 +332,7 @@ CREATE TABLE `task` (
   KEY `user_id` (`user_id`),
   CONSTRAINT `task_ibfk_1` FOREIGN KEY (`department_id`) REFERENCES `department` (`id`),
   CONSTRAINT `task_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -507,4 +507,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-05-31 20:52:15
+-- Dump completed on 2019-06-03 14:12:00
