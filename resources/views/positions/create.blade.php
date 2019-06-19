b673a56b7c92c69524e45c12acc0b1f1fb8802b8
Agon Salihu <as40386@ubt-uni.net>
2019-06-19 13:15:07 +0200

    Positions

---
+++ resources/views/positions/create.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+@section('content')
+<div class="content">
+        <div class="container-fluid">
+            <div class="row11">
+                <div class="col-md-10">
+                    <div class="header" style="text-align:center;margin-bottom:20px;color:#2b7691;">
+                                <h4 class="title" >New Position</h4>
+                    </div>
+                    <div class="card1">
+                        
+                        <div class="content">
+                        <form action="{{action('PositionController@store')}}" method="POST">
+                            {{ csrf_field() }}
+                            @method('POST')
+                                <div class="row1">
+                                    <div class="col-md-5">
+                                        <div class="form-group1">
+                                            <label>Position name</label>
+                                        <input type="text" name="name" class="form-control" placeholder="Position Name" value="">
+                                        </div>
+                                    </div>
+                                                                           
+                                                               
+                                </div>
+
+                                <div class="row1">
+                                    <div class="col-md-6">
+                                        <label>Department</label>
+                                        <select class="form-control select md-form" name="position_id">
+                                                <option value="" disabled selected>Department</option>
+                                                @foreach ($department as $department)                                                    
+                                                    <option value="{{$department->id}}">{{$department->name}}</option>
+                                                @endforeach                                            
+                                        </select>
+                                    </div>  
+                                                                                                
+                                </div>                                
+                                <div class="row">                                    
+                                    
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-2">
+                                        <input style="margin-top:15px;" class="btn btn-outline-success col-md-12" type="submit">
+                                    </div>                        
+                                </div>                            
+                                <div class="clearfix"></div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div>
+@endsection
